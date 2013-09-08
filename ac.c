22932dbcf5d98e253aec6a15aa10621e41536e18
Chosko <ruben.caliandro@gmail.com>
2013-09-08 15:44:40 +0200

    init

---
+++ ac.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+// emits a binary symbol with P(X=0) = p
+unsigned char emit_source_symbol(double p);
+
+unsigned char emit_source_symbol(double p){
+
+}
+
+int main(int argc, char *argv[]){
+  return 0;
+}
---
+++ makefile
@@ -0,0 +1,9 @@
+compile:
+	gcc -o ac.out ac.c
+
+run:
+	gcc -o ac.out ac.c
+	ac
+
+clean:
+	rm ac.out
